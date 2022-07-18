25a24b171a9810aa0bedbfe6e74ee9cfd89e3ebd
Mufufu <ekikz1997@gmail.com>
2022-07-18 07:34:01 +0000

    added model

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,31 +71,34 @@ def predict(data):
 
 if __name__ == "__main__":
     # root = Path(__file__).parents[1]
-    st.title("Suicide Predictor")
+    st.title("Psychology - Data Augmentation | Suicide Prediction")
     st.image("https://cms.qz.com/wp-content/uploads/2018/08/suicide-prediction-animated-final.gif?quality=75&strip=all&w=1200&h=630&crop=1",width = 800)
-    nav = st.sidebar.radio("Navigation",["Home","Contribute", "About me", "Contact me"])
+    nav = st.sidebar.radio("Menu",["Home","Dataset", "Background", "Contact"])
     if nav == "Home":
         home(data)
         predict(data)
         
 
-    if nav == "Contribute":
-        st.warning("Contribution to the dataset is not developed yet. that's why no submit button")
+    if nav == "Dataset":
+        st.warning("Alpha Development . Heavily Under Progress")
         st.header("Contribute to our dataset")
         text1 = st.text_area("Enter the Sentence")
         label1 = st.selectbox("Select the class",["suicide","non-suicide"],index = 0)
-        # if st.button("submit"):
-        #     #check if we are not reading the dataset
-        #     add_lst = {"Unnamed: 0": [numm+1], "text":[text1],"class":[label1]}
-        #     add_lst = pd.DataFrame(add_lst)
-        #     add_lst.to_csv(root/"new_Suicide_Detection.csv",mode='a',header = False,index= False)
-        #     st.success("Submitted")
-    if nav == "About me":
-        st.header("About me")
-        st.write("I am Akash Maurya, Currently serving in Cognizant as a Programmer Analyst Trainee. I am an IBM-certified Data Scientist. I love playing with data and drawing insights for businesses. I am an expert in Python, Machine Learning, Artificial Intelligence, and Data Science. I have been in this industry for the last 4 years. Currently serving as a part-time instructor and Expert of Machine Learning and Artificial Intelligce.")
-        st.write("I am an independent Data Scientist. have experience on varius freelance projects from the field of data science and Machine learning. Deep learning is one of my favorite subject. you can see my achivement at")
-        
-        st.subheader('View my Achivements')
+    if st.button("submit"):
+             #check if we are not reading the dataset
+             add_lst = {"Unnamed: 0": [numm+1], "text":[text1],"class":[label1]}
+             add_lst = pd.DataFrame(add_lst)
+             add_lst.to_csv(root/"new_Suicide_Detection.csv",mode='a',header = False,index= False)
+             st.success("Submitted . Thankyou for your contribution")
+    if nav == "Background":
+        st.header("Objective")
+        st.write("This machine learning created as an alteration series . Using Natural Language Process and Data Augmented .")
+        st.write("It include about how we imagined our self on the other side based on positivity effect .")
+        st.header("Subjective")
+        st.write("This machine learning created as an alteration series . Using Linear Regression and Data augmented .")
+        st.write("It include about how we imagined our self on the other side based on positivity effect .")
+
+        st.subheader('Technologies stack')
         
         #components.iframe("https://docs.streamlit.io/en/latest")
         col1, col2, col3 = st.columns(3)
@@ -103,56 +106,52 @@ if __name__ == "__main__":
         col7, col8, col9 = st.columns(3)
         with col1:
             st.text("""
-            Data Science Professional
-            Certificate
+            NLTK
+            Natural Language Processing
             """)
             components.html("""<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="c71eb334-9ced-4463-955d-dded93f6f364" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>""")
         with col2:
             st.text("""
-            Applied Data Science 
-            Specialization
+            Matplotlib 
+            for Plotting
             """)
             components.html("""<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="838f5fcb-b703-46e6-aecc-dec1942a2b58" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>""")
         with col3:
             st.text("""
-            Applied Data Science 
-            Capstone
+            NumPy
+            
             """)
             components.html("""<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="c389a1bc-92fa-4bf6-ae9f-3b49127087a8" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>""")
         with col4:
             st.text("""
-            Data Analysis with 
-            Python
+            SciKit-Learn
+            
             """)
             components.html("""<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="2ef7d03d-4263-45c9-9a94-9f4b16f656fc" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>""")
         with col5:
             st.text("""
-            Data Visualization with 
-            Python
+            Pandas
+            
             """)
             components.html("""<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="47e710e8-35b4-4bda-b2b7-dde82ac82570" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>""")
         with col6:
             st.text("""
-            Machine Learning with 
-            Python
+            Streamlit
+            
             """)
             components.html("""<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="9c81971e-e2b0-4c16-beef-7d35609723f5" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>""")
-        with col7:
-            st.text("""
-            Databases and SQL for 
-            Data Science
-            """)
-            components.html("""<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="f884aac7-0b7d-4e60-9d10-d81f2d00f8eb" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>""")
     
     
-    if nav == "Contact me":
-        st.header("Contact me")
+    if nav == "Contact":
+        st.header("About Developer")
+        st.write("Rizki Febriansyah is an undergraduate student with a strong passion on technologies , He does some research on college and work at Open Source Software as React Developer .")
+        st.write("He has experience as React Developer and Machine Learning Researcher . Determined to continue moving forward , devoted with some promise to get a brighter tomorrow.")
+        st.header("Contact")
         st.markdown("""
-        **mail id:** akashmaurya517@gmail.com\n
-        **Contact:** 8115739862, 8172886817\n
-        **Linkein:** https://www.linkedin.com/in/akash-maurya-347911164\n
-        **github:** https://github.com/akashmaurya517\n
-        **Twitter:** https://twitter.com/Akashma35277748\n
-        **Credly:** https://www.credly.com/users/akash-maurya.c20a97c8/badges#\n
+        **Mail:** ekikz1997@gmail.com\n
+        **Phone:** (+62) 853-1232-9672\n
+        **Linkedin:** https://www.linkedin.com/in/rizki-febriansyah97\n
+        **GitHub:** https://github.com/RFebrians\n
+        **Google Dev:** https://g.dev/zegveld\n
         """)
         
